5d3d1818885e29045d98645b076dbdddb2a5fe9f
Shaswata-codes <shaswatasarkar05@gmail.com>
2025-10-12 13:54:35 +0530

    Firebase Updated

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./profileupdate.css";
 import assets from "../../assets/assets";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const ProfileUpdate = () => {
-  const [image, setImage] = useState(null);
-  const [uploadedUrl, setUploadedUrl] = useState(null);
+    const  navigate = useNavigate();
+    const [image, setImage] = useState(null);
+    const [uploadedUrl, setUploadedUrl] = useState(null);
     const [loading, setLoading] = useState(false);
+    const[name, setName] = useState("");
+    const[bio, setBio] = useState("");
+    const[uid, setUid] = useState("");
+    const [prevImage, setPrevImage] = useState("");
+
+    useEffect(()=>{
+        onAuthStateChanged(auth, async(user)=>{
+            if(user){
+                setUid(user.uid)
+                const docRef = doc(db, "users", user.uid);
+                const docSnap = await getDoc(docRef);
+                if(docSnap.data.name){
+                    setName(docSnap.data.name)
+                }
+                if(docSnap.data.bio){
+                    setName(docSnap.data.bio)
+                }
+                if(docSnap.data.avatar){
+                    setPrevImage((await docSnap).data().avatar)
+                }
+            }
+            else{
+                navigate('/')
+            }
+        })
+    },[])
 
     const handleImageChange = async (e) => {
     const file = e.target.files[0];
@@ -68,9 +99,9 @@ const ProfileUpdate = () => {
             />
             {loading ? "Uploading..." : "Upload Profile Image"}
             </label>
+            <input onChange={(e)=>setName(e.target.value)} value={name} type ="text" placeholder="Your Name" required/>
 
-            <input type="text" placeholder="First Name" required />
-            <textarea placeholder="Write profile bio" required></textarea>
+            <textarea onChange={(e)=> setBio(e.target.bio)} value={bio} placeholder="Write profile bio" required></textarea>
 
             <button type="submit">Save</button>
         </form>
