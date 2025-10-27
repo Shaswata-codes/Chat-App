8c81000989f82e15f2a658e09002b1c2ac4ce628
Shaswata-codes <shaswatasarkar05@gmail.com>
2025-10-27 17:46:13 +0530

    chnages

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import "./Profileupdate.css";
+import "./ProfileUpdate.css";
 import assets from "../../assets/assets";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../../config/firebase";
